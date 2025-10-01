7719e313f190372b75fdd1bb2acd1a33970c1733
MichaelFar <rarrafarrar@gmail.com>
2025-10-01 10:42:37 -0700

    Added first draft of dragging

--- Assets/Scripts/DraggableBody.cs
+++ Assets/Scripts/DraggableBody.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DraggableBody : MonoBehaviour
@@ -8,14 +9,26 @@ public class DraggableBody : MonoBehaviour
 
     public Rigidbody rb;
     public Transform destinationObject;
+    public float distanceLimit = 5.0f;
+    public float dragForce = 3.0f;
     void Start()
     {
        
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        rb.MovePosition(destinationObject.position);
+        float distance_to_object = Vector3.Distance(transform.position, destinationObject.position);
+        Vector3 direction_to_object = destinationObject.position - transform.position;
+
+        direction_to_object = direction_to_object.normalized;
+
+        if (distance_to_object > distanceLimit)
+        {
+
+            rb.AddForce(direction_to_object * dragForce);
+            //rb.MovePosition(destinationObject.position);
+        }
     }
 }
