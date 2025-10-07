1a9ee29289b06e8cf3581dc895bb74e8a7dd16fa
MichaelFar <rarrafarrar@gmail.com>
2025-10-07 14:51:58 -0700

    Tweaked draggable body to pay attention to two targets, added basic rope prototype without a good tool to make it, need to do that, commit before attaching rope to friend

--- Assets/Scripts/CollisionDamageLerper.cs
+++ Assets/Scripts/CollisionDamageLerper.cs
@@ -14,6 +14,8 @@ public class CollisionDamageLerper : MonoBehaviour
     public Material fullHealthMaterial;
     public HealthBar healthBar;
 
+    public float forceThreshold = 3.0f; //If the force of impact is higher than this, damage will be taken
+
     //Proto Health System, will be changed for per material and body part
     public float maxHealthLevel = 100.0f; //Most healthy at 0.0, might change logic to reverse if unintuitive
     private float currentHealthLevel = 0.0f;
@@ -33,10 +35,13 @@ public class CollisionDamageLerper : MonoBehaviour
    
     public void DamageMesh(float damage_from_impact)
     {
-        currentHealthLevel -= damage_from_impact;
-        float normalized_health = currentHealthLevel / maxHealthLevel;
-        rend.material.Lerp(damagedMaterial, fullHealthMaterial, normalized_health);
-        healthBar.SetHealthBarValue(normalized_health);
+        if (damage_from_impact > forceThreshold)
+
+            currentHealthLevel -= damage_from_impact;
+            float normalized_health = currentHealthLevel / maxHealthLevel;
+            rend.material.Lerp(damagedMaterial, fullHealthMaterial, normalized_health);
+            if(healthBar != null)
+                healthBar.SetHealthBarValue(normalized_health);
     }
     
     
--- Assets/Scripts/DraggableBody.cs
+++ Assets/Scripts/DraggableBody.cs
@@ -10,8 +10,10 @@ public class DraggableBody : MonoBehaviour
     public Rigidbody rb;
     public Renderer rend;
     public CollisionDamageLerper damageLerper;
-    public Transform destinationObject;
-    public float distanceLimit = 5.0f;
+    public Transform destinationObject1;
+    public Transform destinationObject2;
+    public float distanceLimit1 = 5.0f;
+    public float distanceLimit2 = 5.0f;
     public float dragForce = 3.0f;
 
     
@@ -27,23 +29,42 @@ public class DraggableBody : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        float distance_to_object = Vector3.Distance(transform.position, destinationObject.position);
-        Vector3 direction_to_object = destinationObject.position - transform.position;
+        float distance_to_object1 = Vector3.Distance(transform.position, destinationObject1.position);
+        Vector3 direction_to_object1 = destinationObject1.position - transform.position;
 
-        direction_to_object = direction_to_object.normalized;
+        
 
-        if (distance_to_object > distanceLimit)
+        direction_to_object1 = direction_to_object1.normalized;
+        
+        
+
+        if (distance_to_object1 > distanceLimit1)
         {
 
-            rb.AddForce(direction_to_object * dragForce);
+            rb.AddForce(direction_to_object1 * dragForce * distance_to_object1);
+            
             //rb.MovePosition(destinationObject.position);
+        }
+
+        if (destinationObject2 != null)
+        {
+            float distance_to_object2 = Vector3.Distance(transform.position, destinationObject2.position);
+            Vector3 direction_to_object2 = destinationObject2.position - transform.position;
+
+            direction_to_object2 = direction_to_object2.normalized;
+
+            if (distance_to_object2 > distanceLimit2)
+            {
+
+                rb.AddForce(direction_to_object2 * dragForce * distance_to_object2);
+                //rb.MovePosition(destinationObject.position);
+            }
         }
     }
     private void OnCollisionEnter(Collision collision)
     {
         print("Testing collision on rigid body");
-        if (GetMagnitudeOfCollison() > 5.0f) 
-            damageLerper.DamageMesh(GetMagnitudeOfCollison());
+        damageLerper.DamageMesh(GetMagnitudeOfCollison());
 
     }
     private float GetMagnitudeOfCollison()
